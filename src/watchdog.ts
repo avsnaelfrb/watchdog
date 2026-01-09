c24ec55b8689aa2aeaa7b57c31922a786e50db75
avsnaelfrb <annabhaniy@gmail.com>
2026-01-09 14:32:03 +0800

    feat: add cron for automation notif in telegram

--- src/watchdog.ts
+++ src/watchdog.ts
@@ -1,3 +1,24 @@
-// import fs from 'fs'
+import fs from 'fs'
+import type { Items } from './scraper.js';
 
-// export const checkForDiscount = (livedata:)
+export const compareCount = (oldData: Items[], newData: Items[]): any => {
+    let alerts : string[] = []
+    
+    try {
+        newData.forEach(newItem => {
+            const oldItem = oldData.find(item => item.link === newItem.link)
+    
+            if (!oldItem) {
+                alerts.push(`Ada item terbaru: \n Name: ${newItem.name} \n Harga: ${newItem.cost} \n`)
+            } else if (newItem.cost < oldItem.cost) {
+                const totalDiskon = oldItem.cost - newItem.cost
+                alerts.push(`Diskon pada ${newItem.name}: \n Diskon sebesar: ${totalDiskon} \n Harga lama: ${oldItem.cost} => ${newItem.cost} \n`)
+            } else if (newItem.cost > oldItem.cost) {
+                alerts.push(`\nHarga "${newItem.name}" naik ke:\n${newItem.cost}`)
+            }
+        });
+        return alerts
+    } catch (error) {
+        return []
+    }
+}
